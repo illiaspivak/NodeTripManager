c84ba0387a62de1f3805ae7b911a63c089dde799
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-22 11:51:15 +0200

    Project server NodeJS

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const mongodb = require('mongodb')
+
+const connectionURL = 'mongodb://localhost:27017'
+
+const app = express()
+const MongoClient = mongodb.MongoClient
+
+app.get('', (req,res)=> {
+    res.send('Hello! I am your NoteJS server!')
+})
+
+app.get('/about', (req,res)=>{
+    res.send('<h1>Server trip manager<\h1>')
+})
+
+app.get('/author', (req,res)=>{
+    res.send('"author": "Ilya"')
+})
+
+app.get('/trip', (req,res)=>{
+    MongoClient.connect(connectionURL, {useNewURLParser: true}, (error, client) =>{
+        if(error){
+            return console.log('Unable to connect to database')
+        
+        }
+        console.log('Connection succesfully')
+
+        const db = client.db('tripplanner');
+
+        db.collection('trips').find().toArray( (err, result) =>{
+            if(err) throw err;
+            console.log(result)
+            res.send(result);
+        })
+
+       // const result = db.collection('trip').find()
+       // console.log(result)
+       // res.send(result)
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000')
+})
