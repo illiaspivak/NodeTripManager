4becebe8bd1a96fb9e320ab346ba61dde895aef1
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-28 11:45:23 +0200

    add js file POST request

---
+++ web/js/script.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById("addTrip");
+
+btn.addEventListener("click", () =>{
+    const title = document.getElementById("tripTitle").value;
+    const place = document.getElementById("place").value;
+    const distance = parseInt(document.getElementById("distance").value);
+    const difficultyLevel = parseInt(document.getElementById("difficultyLevel").value);
+    
+    const object = {title, place, distance, difficultyLevel};
+    console.log(object);
+    const jsonData=JSON.stringify(object);
+    console.log(jsonData);
+
+    let trip = {"title": title, "place": place, "distance":distance, "difficultyLevel":difficultyLevel};
+
+    fetch('http://localhost:3000/trip/new', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+        },
+        mode:"no-cors",
+        body: object,
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+         console.error('Error:', error);
+     });
+})
