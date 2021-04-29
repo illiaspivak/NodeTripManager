2c8cd583dd1704eb6ea08ed692c328caf36b29ca
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-29 12:12:46 +0200

    show all trips

--- web/js/trip.js
+++ web/js/trip.js
@@ -4,11 +4,26 @@ $( ()=>{
         url: "http://localhost:3000/trip",
         type: "get",
         
-        success: (result)=>{
-            console.log(result);
+        statusCode: { 
+            200: (result)=>{ 
+                for(var index in result){
+                    const title = result[index].title
+                    const place = result[index].place
+                    const distance = result[index].distance
+                    const difficultyLevel = result[index].difficultyLevel
+                    const placeVisited = result[index].placeVisited
+        
+                    var text = title + " (" + place + ") distance: " + 
+                    distance + " difficulty level: " + difficultyLevel +
+                    " place visited: " + placeVisited;
+                    console.log(text);
+                    var newElement = $("<div></div>").text(text);
+                    var elementBR=$("<br/>");
+                    $("#parrent").append(newElement, elementBR); 
+                }
+            },
+            400: (err)=>{ console.log('Bad request, ')},
+            404: (err)=>{ console.log('Not found !')}
         },
-        error:  (err)=>{
-            console.log("Error: ",err);
-        }
     })
 });
